3e9f1888d02b129028a8939217c5e1a4cf551944
Amlesh <amleshkr396@gmail.com>
2024-10-13 22:58:03 +0530

    changed cors origin

--- src/Components/Login/login.jsx
+++ src/Components/Login/login.jsx
@@ -30,7 +30,7 @@ const Login = () => {
     setLoading(true);
 
     try {
-      const url = "https://tree-view-app-backend.onrender.com";
+      const url = "https://inverntory-insight.vercel.app";
       const response = await axios.post(`${url}/user/login`, formData);
       const { token, success } = response.data;
 
